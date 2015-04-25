96d63834087f9584f5733a315e6ad46ac05c6347
Diego Calle <dnetix@gmail.com>
2015-04-24 20:10:33 -0500

    Elimino los archivos del repositorio que sobraban

--- xyCoordinates.m
+++
@@ -1,7 +0,0 @@
-function [ x,y ] = xyCoordinates(index, m)
-%POSITION Summary of this function goes here
-%   Detailed explanation goes here
-    x = mod(index, m);
-    y = floor(index / m) + 1;
-end
-
